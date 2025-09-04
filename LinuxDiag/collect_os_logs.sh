b37c5e9bfffa4aef692bcd4bf6dfa5e91fec42f3
rl-msft <rafidl@microsoft.com>
2025-09-04 08:51:50 +0000

    Standazing file extension, out for nexus and info for Linux

--- LinuxDiag/collect_os_logs.sh
+++ LinuxDiag/collect_os_logs.sh
@@ -17,11 +17,11 @@ outputdir=$PWD/output
 #Check if I am running on host/systemd
 if (echo "$(readlink /sbin/init)" | grep systemd >/dev/null 2>&1); then
 	echo "$(date -u +"%T %D") Collecting dmesg log, journalctl, system logs..." | tee -a $pssdiag_log
-	dmesg > $outputdir/${HOSTNAME}_os_dmesg.out
-	journalctl | tail -n1000 > $outputdir/${HOSTNAME}_os.journalctl.out
-	journalctl -u mssql-server > $outputdir/${HOSTNAME}_host_instance_journalctl.out
-	journalctl -u docker > $outputdir/${HOSTNAME}_os_docker.journalctl.out
-	journalctl -u podman > $outputdir/${HOSTNAME}_os_podman.journalctl.out
+	dmesg > $outputdir/${HOSTNAME}_os_dmesg.info
+	journalctl | tail -n1000 > $outputdir/${HOSTNAME}_os.journalctl.info
+	journalctl -u mssql-server > $outputdir/${HOSTNAME}_host_instance_journalctl.info
+	journalctl -u docker > $outputdir/${HOSTNAME}_os_docker.journalctl.info
+	journalctl -u podman > $outputdir/${HOSTNAME}_os_podman.journalctl.info
 
 	# this is required to figure out version of distro so that System log files are collected appropriately in the following case statement
 	linuxdistro=`cat /etc/os-release | grep -i '^ID=' | head -n1 | awk -F'=' '{print $2}' | sed 's/"//g'`
@@ -50,7 +50,7 @@ get_servicemanager_and_sqlservicestatus "host_instance"
 if [[ "${servicemanager}" == "supervisord" ]]; then
 	echo -e "$(date -u +"%T %D") Collecting Supervisor, provisioner, agent logs..." | tee -a $pssdiag_log
 	
-	dmesg > $outputdir/${HOSTNAME}_dmesg.out
+	dmesg > $outputdir/${HOSTNAME}_dmesg.info
 
 	# this is required to figure out version of distro so that System log files are collected appropriately in the following case statement
 	linuxdistro=`cat /etc/os-release | grep -i '^ID=' | head -n1 | awk -F'=' '{print $2}' | sed 's/"//g'`
